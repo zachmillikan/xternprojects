39b8b9118442d5d60452cd6f1d1a8eabde576ec1
Zach <millikanzach@gmail.com>
2016-06-13 13:47:50 -0400

    Initial commit with meganote seed.

---
+++ app/app.js
@@ -0,0 +1,22 @@
+(function() {
+  var app = angular.module('meganote', [
+    'ui.router'
+  ]);
+
+  function config($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/notes');
+
+    $stateProvider
+
+      .state('notes', {
+        url: '/notes',
+        template: '<h1>Meganote</h1><p>{{ message }}</p>',
+        controller: function($scope) {
+          $scope.message = "Welcome to Meganote!";
+        }
+      });
+  }
+
+  config['$inject'] = ['$stateProvider', '$urlRouterProvider'];
+  app.config(config);
+})();
